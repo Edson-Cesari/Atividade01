657097e096c788409a1070d5efedd396baeada3e
Edson Cesari <cesariedson2@gmail.com>
2022-10-31 20:40:50 -0300

    Atividade 01 Proz

---
+++ atividade1.py
@@ -0,0 +1,14 @@
+nomeAluno = "Edson"
+nota1 = 7.5
+nota2 = 6.5
+media = (nota1 + nota2)/2; 
+faltas = 3
+
+if faltas > 3 :
+    print("O aluno ",nomeAluno," foi reprovado")
+else :
+    if media < 7 :
+        print("O aluno ",nomeAluno," foi reprovado")
+    else:
+        print("O aluno ",nomeAluno," foi aprovado")
+    
